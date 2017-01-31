b38ce68e7347c19eaeb397e3e6eeca0c44aa0a35
Mike Chastain <mchastain@fuelmedical.com>
2017-01-30 21:42:53 -0800

    moves files to learning_reference dir, adds satori-uuid and gorm demos for reference

--- learning_reference/satori-uuid/satori-uuid-demo.go
+++ learning_reference/satori-uuid/satori-uuid-demo.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// Creating UUID Version 4
 	u1 := uuid.NewV4()
-	fmt.Printf("UUIDv4: %s\n", u1)
+	fmt.Printf("UUIDv4: %d\n", u1)
 
 	// Parsing UUID from string input
 	u2, err := uuid.FromString("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
@@ -16,4 +16,10 @@ func main() {
 		fmt.Printf("Something gone wrong: %s", err)
 	}
 	fmt.Printf("Successfully parsed: %s", u2)
+
+	//	u3 := uuid.NewV5()
+	//	fmt.Printf("UUIDv5: %s\n", u3)
+
+	// mysql likes binary(16) encodeing so probably need to do something like
+	//func (u UUID) MarshalBinary() (data []byte, err error)
 }
