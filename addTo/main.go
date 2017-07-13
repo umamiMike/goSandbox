5cb43daea9c87bdab9304520b82c7a2c7b38f588
Mike Chastain <michaelwchastain@gmail.com>
2017-07-13 15:04:56 -0700

    updates addto file

--- addTo/main.go
+++ addTo/main.go
@@ -1,40 +1,34 @@
 package main
 
-//cli usage
-// addto the/file/path the string you want to add to the file.
-
+/*
+cli usage
+will add a string to a file of your choosing
+I use it to add thoughts to various journals I keep.
+addto the/file/path the string you want to add to the file.
+*/
 import (
-	"fmt"
 	"os"
 	"strings"
 )
-
-func myPrinter(i) {
-	fmt.Printf("%T", i)
-}
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
-
 func main() {
 	infile := os.Args[1]
-	data := os.Args[2:] //everything after the first arg becomes data
-
-	// 	data2 := make(string "\n", data)
-
-	collapsedData := (strings.Join(data, " "))
-	f, err := os.OpenFile(infile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
+	if len(os.Args) > 1 {
+		data := os.Args[2:] //everything after the first arg becomes data
+		collapsedData := (strings.Join(data, " "))
+		f, err := os.OpenFile(infile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		if _, err = f.WriteString(collapsedData); err != nil {
+			panic(err)
+		}
+		check(err)
 	}
-
-	defer f.Close()
-
-	if _, err = f.WriteString(collapsedData); err != nil {
-		panic(err)
-	}
-	check(err)
 }
