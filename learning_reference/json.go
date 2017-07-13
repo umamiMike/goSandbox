863b85074d0528b8105bba09cc350fa01ca08452
Mike Chastain <michaelwchastain@gmail.com>
2017-07-13 14:58:28 -0700

    adds json simple demo

--- learning_reference/json.go
+++ learning_reference/json.go
@@ -21,10 +21,15 @@ func main() {
 	var p2 Person
 	newbs := []byte(`{"First":"James", "Last": "Bond", "wisdom":20}`)
 	json.Unmarshal(newbs, &p2) //unmarshal newbs into p2 object
-
+ 
+	var p3 Person.UnmarshalJSON([]byte(`{"First":"Mycroft", "Last": "Holmes","wisdom":40}`)
 	fmt.Println("-------")
 	fmt.Println(p2.First) //proof by accessing values
 	fmt.Println(p2.Last)
 	fmt.Println(p2.Age) //even unmarshalling json tag wisdom into Age
 	fmt.Printf("%T \n", p2)
+}
+func (p *Person) UnmarshalJSON(data []byte) error {
+json.Unmarshal(data, &p)
+return nil
 }
