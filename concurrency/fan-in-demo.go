cfae85346661bd4f4268b31064f70c72614cb861
Mike Chastain <michaelwchastain@gmail.com>
2017-08-08 17:33:02 -0700

    adds fan in demo to concurrency

---
+++ concurrency/fan-in-demo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func producer(ch chan int, d time.Duration) {
+	var i int
+	r := rand.New(rand.NewSource(99))
+	for {
+		ch <- i
+		i++
+		rantime := time.Duration(r.Intn(3))
+		fmt.Println(rantime)
+		time.Sleep(rantime + d)
+	}
+}
+
+func reader(out chan int) {
+	for x := range out {
+		fmt.Println(x)
+	}
+}
+
+func main() {
+
+	ch := make(chan int)
+	out := make(chan int)
+	go producer(ch, 100*time.Millisecond)
+	go producer(ch, 250*time.Millisecond)
+	go reader(out)
+	for i := range ch {
+		out <- i
+	}
+}
