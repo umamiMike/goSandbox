f0b1f6ac18a217664e0d3fa6668df12d8e9dcf5c
Michael Wilding <michael.w.chastain@gmail.com>
2020-01-29 12:43:17 -0800

    updates adding name. playing around

--- concurrency/fan-in-demo.go
+++ concurrency/fan-in-demo.go
@@ -3,33 +3,48 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
-func producer(ch chan int, d time.Duration) {
+/*
+ * takes a channel and a duration
+ inits an interger,
+ writes to channel
+ increments integer
+ sleeps for a random amount of time in seconds
+*/
+func producer(ch chan string, d time.Duration, name string) {
 	var i int
 	r := rand.New(rand.NewSource(99))
 	for {
-		ch <- i
+		ch <- strconv.Itoa(i) + name
 		i++
-		rantime := time.Duration(r.Intn(6)) * time.Second
-		fmt.Println("I sleep for this long:", rantime)
+		rantime := time.Duration(r.Intn(2)) * time.Second
 		time.Sleep(rantime + d)
 	}
 }
 
-func reader(out chan int) {
+/*
+ * takes an int channel and prints out x from the channel indefinitely
+
+ */
+func reader(out chan string) {
 	for x := range out {
 		fmt.Println(x)
 	}
 }
 
+/*
+ * makes to channels
+ * spawns 3 producers
+ */
 func main() {
 
-	ch := make(chan int)
-	out := make(chan int)
-	go producer(ch, 100*time.Millisecond)
-	go producer(ch, 250*time.Millisecond)
+	ch := make(chan string)
+	out := make(chan string)
+	go producer(ch, 10*time.Millisecond, "barf")
+	go producer(ch, 25*time.Millisecond, "bag")
 	go reader(out)
 	for i := range ch {
 		out <- i
