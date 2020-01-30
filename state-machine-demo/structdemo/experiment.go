34dc94baaf0f592c8d9d610d042694f82c266d83
Michael Wilding <michael.w.chastain@gmail.com>
2020-01-29 16:01:58 -0800

    play with webview WIP, updates state machine demo deletes unneeded file

--- state-machine-demo/structdemo/experiment.go
+++ state-machine-demo/structdemo/experiment.go
@@ -10,50 +10,95 @@ import (
 	"time"
 )
 
-func main() {
-	fsm := fsm.NewFSM(
+/*
+ *type Event struct {
+ *    // FSM is a reference to the current FSM.
+ *    FSM *FSM
+ *
+ *    // Event is the event name.
+ *    Event string
+ *
+ *    // Src is the state before the transition.
+ *    Src string
+ *
+ *    // Dst is the state after the transition.
+ *    Dst string
+ *
+ *    // Err is an optional error that can be returned from a callback.
+ *    Err error
+ *
+ *    // Args is a optinal list of arguments passed to the callback.
+ *    Args []interface{}
+ *    // contains filtered or unexported fields
+ *}
+ *
+ */
+type Work struct {
+	To  string
+	FSM *fsm.FSM
+}
+
+func newWork(to string) *Work {
+
+	w := &Work{}
+	w.FSM = fsm.NewFSM(
 		"idle",
 		fsm.Events{
-			{Name: "scan", Src: []string{"idle"}, Dst: "scanning"},
-			{Name: "working", Src: []string{"scanning"}, Dst: "scanning"},
-			{Name: "situation", Src: []string{"scanning"}, Dst: "scanning"},
-			{Name: "situation", Src: []string{"idle"}, Dst: "idle"},
-			{Name: "finish", Src: []string{"scanning"}, Dst: "idle"},
+			{Name: "start", Src: []string{"stop"}, Dst: "start"},
+			{Name: "stop", Src: []string{"start", "stop", "continue"}, Dst: "stopped"},
+			{Name: "continue", Src: []string{"start", "stopped", "continue"}, Dst: "stop"},
 		},
 		fsm.Callbacks{
-			"scan": func(e *fsm.Event) {
-				fmt.Println("after_scan: " + e.FSM.Current())
-			},
-			"working": func(e *fsm.Event) {
-				fmt.Println("working: " + e.FSM.Current())
-			},
-			"situation": func(e *fsm.Event) {
-				fmt.Println("situation: " + e.FSM.Current())
-			},
-			"finish": func(e *fsm.Event) {
-				fmt.Println("finish: " + e.FSM.Current())
+			"enter_state": func(e *fsm.Event) { w.enterState(e) },
+			"leave_state": func(e *fsm.Event) { w.leaveState(e) },
+			"leave_stop": func(e *fsm.Event) {
+				w.leaveStop(e)
 			},
 		},
 	)
+	return w
+}
+func (d *Work) leaveStop(e *fsm.Event) {
+
+	fmt.Println(`
+*************************
+you totally entered the stop state
+*************************
+`)
+}
+
+func (d *Work) leaveState(e *fsm.Event) {
+	fmt.Println("you are about to leave:  ", e.Src)
+	fmt.Println("you will be in ", e.Event)
+}
+
+func (d *Work) enterState(e *fsm.Event) {
+	fmt.Println("you were in ", e.Src)
+	fmt.Println("now you are in ", e.Event)
+	fmt.Printf("and the destination is:   %s\n", e.Dst)
+}
+
+func main() {
+	work := newWork("poop")
+
 	r := bufio.NewScanner(os.Stdin)
-	fmt.Println(fsm.Current())
+
+	fmt.Println("your current state is: ", work.FSM.Current())
+
 	for {
 		time.Sleep(1 * time.Second)
-		err := fsm.Event("scan")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Print("what is the next event? ")
 
 		for r.Scan() {
 			text := r.Text()
-			time.Sleep(3 * time.Second)
-			fmt.Println("3:" + fsm.Current())
+			time.Sleep(1 * time.Millisecond)
 
-			err = fsm.Event(text)
+			err := work.FSM.Event(text)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("error:", err)
 			}
+			fmt.Println("\n\nyour current state is: ", work.FSM.Current())
+			fmt.Println("available transitios are: ", work.FSM.AvailableTransitions())
+			fmt.Print("What is your next move?:")
 
 		}
 
