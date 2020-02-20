eba062baea15bfb6854aaaf507d8037dc630642b
Michael Wilding <michael.w.chastain@gmail.com>
2020-02-20 12:02:41 -0800

    more useful update to gorm db play using sqlite

--- learning_reference/gorm/main.go
+++ learning_reference/gorm/main.go
@@ -1,37 +1,75 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Product struct {
 	gorm.Model
-	Code  string
-	Price uint
+	Name string
+	Code string
+	Cost uint
+}
+
+type Service struct {
+	gorm.Model
+	Name string
+	Code string
+	Cost uint
 }
 
 func main() {
-	db, err := gorm.Open("sqlite3", "terst.db")
+
+	rand.Seed(time.Now().UnixNano())
+
+	//creates db if doesnt exist
+	db, err := gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		panic("failed to connect database")
 	}
 	defer db.Close()
 
 	// Migrate the schema
+	if !db.HasTable(&Product{}) {
+		db.CreateTable(&Product{})
+	}
 	db.AutoMigrate(&Product{})
 
 	// Create
-	db.Create(&Product{Code: "L1212", Price: 1000})
+	db.Create(&Product{Code: "L1212", Cost: 1000})
 
 	// Read
-	var product Product
-	db.First(&product, 1)                   // find product with id 1
-	db.First(&product, "code = ?", "L1212") // find product with code l1212
+	var productWithCode Product
+	db.First(&productWithCode, 1) // find product with id 1
+	fmt.Println(productWithCode.Code)
+	db.First(&productWithCode, "code = ?", "L1212") // find product with code l1212
+	fmt.Println(productWithCode.Cost)
 
-	// Update - update product's price to 2000
-	db.Model(&product).Update("Price", 2000)
+	//// Update - update product's price to 2000
+	//pr := Product{}
+	db.First(&productWithCode).Update("Cost", rand.Intn(1000))
+	fuck := func(p *Product) []byte {
+		v, _ := json.MarshalIndent(p, " ", " ")
+		return v
+	}(&productWithCode)
 
+	fmt.Println(string(fuck))
+	//db.Save(&product)
+
+	//fmt.Println(product.Get(db))
+
+	db.Create(&Product{Code: "L1212", Cost: 1000})
 	// Delete - delete product
 	//	db.Delete(&product)
+}
+
+func (p *Product) Get(db *gorm.DB) (interface{}, bool) {
+	return db.Where(&p).Get("Price")
+
 }
