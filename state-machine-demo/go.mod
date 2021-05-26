664b67103cd34be94f52eb82d275d69f7407ad26
Michael Wilding <heymichaelwilding@gmail.com>
2021-05-25 22:03:18 -0700

    updates

--- state-machine-demo/go.mod
+++ state-machine-demo/go.mod
@@ -2,4 +2,7 @@ module github.com/umamimike/goSandbox/state-machine-demo/m/v2
 
 go 1.13
 
-require github.com/looplab/fsm v0.1.0
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/looplab/fsm v0.1.0
+)
--- state-machine-demo/simple.go
+++ state-machine-demo/simple.go
@@ -2,30 +2,59 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/looplab/fsm"
 )
 
-func main() {
-	fsm := fsm.NewFSM(
-		"closed",
+type Door struct {
+	To  string
+	FSM *fsm.FSM
+}
+
+func NewDoor(to string) *Door {
+	d := &Door{
+		To: to,
+	}
+
+	d.FSM = fsm.NewFSM(
+		"closed", //initial state
 		fsm.Events{
 			{Name: "open", Src: []string{"closed"}, Dst: "open"},
-			{Name: "splay", Src: []string{"closed"}, Dst: "splayed"},
-			{Name: "close", Src: []string{"open", "splay"}, Dst: "closed"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
 		},
-		fsm.Callbacks{},
+		fsm.Callbacks{
+			"enter_state": func(e *fsm.Event) { d.enterState(e) },
+			"leave_state": func(e *fsm.Event) { d.leaveState(e) },
+			"before_open": func(e *fsm.Event) { d.beforeOpen(e) },
+		},
 	)
-	fmt.Println(fsm.Current())
-	err := fsm.Event("open")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println()
-	fmt.Println("The current state of the door is: ", fsm.Current(), ", can the door close?", fsm.Can("close"))
-	err = fsm.Event("close")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("The current state of the door is: ", fsm.Current(), ", can the door close?", fsm.Can("close"))
 
+	return d
+}
+
+func (d *Door) enterState(e *fsm.Event) {
+	fmt.Printf("The door to %s is %s\n", d.To, e.Dst)
+}
+
+func (d *Door) leaveState(e *fsm.Event) {
+	fmt.Printf("leaving %s and on to %s\n", e.Src, e.Dst)
+}
+
+func (d *Door) beforeOpen(e *fsm.Event) {
+	fmt.Printf("the door to %s is %s, and will soon %s\n\n", d.To, e.Src, e.Dst)
+}
+
+func main() {
+	door := NewDoor("hell")
+
+	err := door.FSM.Event("open")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = door.FSM.Event("close")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
